a2fedc353f81b4b472b8c4048a8e177856919ad3
Christine Rose <christinerose@Christines-MacBook-Air.local>
2021-02-18 11:46:02 -0800

    Added more commangs to Grocery_List and created Recent_Movies

--- Grocery_List.sql
+++ Grocery_List.sql
@@ -2,24 +2,41 @@
 Bananas (4)
 Peanut Butter (1)
 Dark Chocolate Bars (2)
+Aisle #
 **/
 
 CREATE TABLE Grocery
 (
     id INTEGER PRIMARY KEY,
     name TEXT ,
-    quantity INTEGER
+    quantity INTEGER,
+    aisle INTEGER
 );
 
 INSERT INTO Grocery
 VALUES
-    (1, "Bananas", 4);
+    (1, "Bananas", 4, 1);
 INSERT INTO Grocery
 VALUES
-    (2, "Peanut Butter", 1);
+    (2, "Peanut Butter", 1, 4);
 INSERT INTO Grocery
 VALUES
-    (3, "Dark Chocolate Bars", 2);
+    (3, "Dark Chocolate Bars", 2, 10);
 
 SELECT *
 FROM Grocery;
+
+/**Select just from one column:**/
+SELECT name
+FROM Grocery;
+
+/**Order by a certain column name:**/
+SELECT *
+FROM Grocery
+ORDER BY name;
+
+/**Filter results by WHERE clause:**/
+SELECT *
+FROM Grocery
+WHERE aisle > 5
+ORDER BY name;
